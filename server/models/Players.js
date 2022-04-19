69c9aadb8cf463acea14e22fde41d770651c0118
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-04-18 19:39:25 -0700

    creating a person Id string

--- server/models/Players.js
+++ server/models/Players.js
@@ -3,4 +3,5 @@ module.exports = (sequelize, DataTypes) => {
     const Player = sequelize.define('Player',{
         firstName: DataTypes.STRING,
         lastName: DataTypes.STRING,
+        personId: DataTypes.STRING,
     }
